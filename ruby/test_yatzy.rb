f82652fb6e3ad0d0205425881a44c31c6b7c85cf
Zachary Wright <zachwr@zillowgroup.com>
2018-03-05 14:16:36 -0500

    Support 4 dice.

--- ruby/test_yatzy.rb
+++ ruby/test_yatzy.rb
@@ -5,12 +5,19 @@ class YatzyTest < Test::Unit::TestCase
   def test_chance
     assert_equal 15, Yatzy.new(2, 3, 4, 5, 1).chance
     assert_equal 16, Yatzy.new(3, 3, 4, 5, 1).chance
+
+    assert_equal 14, Yatzy.new(2, 3, 4, 5).chance
+    assert_equal 15, Yatzy.new(3, 3, 4, 5).chance
   end
 
   def test_yatzy
     assert_equal 50, Yatzy.new(4, 4, 4, 4, 4).yatzy
     assert_equal 50, Yatzy.new(6, 6, 6, 6, 6).yatzy
     assert_equal 0, Yatzy.new(6, 6, 6, 6, 3).yatzy
+
+    assert_equal 50, Yatzy.new(4, 4, 4, 4).yatzy
+    assert_equal 50, Yatzy.new(6, 6, 6, 6).yatzy
+    assert_equal 0, Yatzy.new(6, 6, 6, 3).yatzy
   end
 
   def test_ones
@@ -18,34 +25,61 @@ class YatzyTest < Test::Unit::TestCase
     assert_equal 2, Yatzy.new(1, 2, 1, 4, 5).ones
     assert_equal 0, Yatzy.new(6, 2, 2, 4, 5).ones
     assert_equal 4, Yatzy.new(1, 2, 1, 1, 1).ones
+
+    assert_equal 1, Yatzy.new(1, 2, 3, 4).ones
+    assert_equal 2, Yatzy.new(1, 2, 1, 4).ones
+    assert_equal 0, Yatzy.new(6, 2, 2, 4).ones
+    assert_equal 4, Yatzy.new(1, 1, 1, 1).ones
   end
 
   def test_twos
     assert_equal 4, Yatzy.new(1, 2, 3, 2, 6).twos
     assert_equal 10, Yatzy.new(2, 2, 2, 2, 2).twos
+    assert_equal 0, Yatzy.new(1, 3, 4, 5, 6).twos
+
+    assert_equal 4, Yatzy.new(1, 2, 3, 2).twos
+    assert_equal 8, Yatzy.new(2, 2, 2, 2).twos
+    assert_equal 0, Yatzy.new(1, 3, 4, 5).twos
   end
 
   def test_threes
     assert_equal 6, Yatzy.new(1, 2, 3, 2, 3).threes
     assert_equal 12, Yatzy.new(2, 3, 3, 3, 3).threes
+    assert_equal 0, Yatzy.new(1, 2, 4, 5, 6).threes
+
+    assert_equal 6, Yatzy.new(1, 2, 3, 3).threes
+    assert_equal 12, Yatzy.new(3, 3, 3, 3).threes
+    assert_equal 0, Yatzy.new(1, 2, 4, 5).threes
   end
 
   def test_fours
     assert_equal 12, Yatzy.new(4, 4, 4, 5, 5).fours
-    assert_equal 8, Yatzy.new(4, 4, 5, 5, 5).fours
     assert_equal 4, Yatzy.new(4, 5, 5, 5, 5).fours
+    assert_equal 0, Yatzy.new(1, 2, 3, 5, 6).fours
+
+    assert_equal 12, Yatzy.new(4, 4, 4, 5).fours
+    assert_equal 4, Yatzy.new(4, 5, 5, 5).fours
+    assert_equal 0, Yatzy.new(1, 2, 3, 5).fours
   end
 
   def test_fives
     assert_equal 10, Yatzy.new(4, 4, 4, 5, 5).fives
     assert_equal 15, Yatzy.new(4, 4, 5, 5, 5).fives
-    assert_equal 20, Yatzy.new(4, 5, 5, 5, 5).fives
+    assert_equal 0, Yatzy.new(1, 2, 3, 4, 6).fives
+
+    assert_equal 5, Yatzy.new(4, 4, 4, 5).fives
+    assert_equal 10, Yatzy.new(4, 4, 5, 5).fives
+    assert_equal 0, Yatzy.new(1, 2, 3, 4).fives
   end
 
   def test_sixes
     assert_equal 0, Yatzy.new(4, 4, 4, 5, 5).sixes
     assert_equal 6, Yatzy.new(4, 4, 6, 5, 5).sixes
     assert_equal 18, Yatzy.new(6, 5, 6, 6, 5).sixes
+
+    assert_equal 0, Yatzy.new(4, 4, 4, 5).sixes
+    assert_equal 6, Yatzy.new(4, 4, 6, 5).sixes
+    assert_equal 18, Yatzy.new(6, 5, 6, 6).sixes
   end
 
   def test_one_pair
@@ -54,19 +88,32 @@ class YatzyTest < Test::Unit::TestCase
     assert_equal 12, Yatzy.new(6, 6, 6, 6, 6).one_pair
     assert_equal 12, Yatzy.new(5, 3, 6, 6, 5).one_pair
     assert_equal 0, Yatzy.new(1, 2, 3, 4, 5).one_pair
+
+    assert_equal 6, Yatzy.new(3, 4, 3, 5).one_pair
+    assert_equal 10, Yatzy.new(5, 5, 3, 3).one_pair
+    assert_equal 12, Yatzy.new(6, 6, 6, 6).one_pair
+    assert_equal 12, Yatzy.new(5, 3, 6, 6).one_pair
+    assert_equal 0, Yatzy.new(1, 2, 3, 4).one_pair
   end
 
   def test_two_pair
     assert_equal 16, Yatzy.new(3, 3, 5, 4, 5).two_pair
-    assert_equal 16, Yatzy.new(3, 3, 5, 5, 5).two_pair
+    assert_equal 20, Yatzy.new(5, 5, 5, 5, 5).two_pair
     assert_equal 0, Yatzy.new(3, 3, 1, 2, 4).two_pair
+
+    assert_equal 16, Yatzy.new(3, 3, 5, 5).two_pair
+    assert_equal 20, Yatzy.new(5, 5, 5, 5).two_pair
+    assert_equal 0, Yatzy.new(3, 3, 1, 2).two_pair
   end
 
   def test_three_of_a_kind
     assert_equal 9, Yatzy.new(3, 3, 3, 4, 5).three_of_a_kind
-    assert_equal 15, Yatzy.new(5, 3, 5, 4, 5).three_of_a_kind
-    assert_equal 9, Yatzy.new(3, 3, 3, 3, 5).three_of_a_kind
+    assert_equal 15, Yatzy.new(5, 5, 5, 5, 5).three_of_a_kind
     assert_equal 0, Yatzy.new(3, 3, 2, 2, 6).three_of_a_kind
+
+    assert_equal 9, Yatzy.new(3, 3, 3, 4).three_of_a_kind
+    assert_equal 15, Yatzy.new(5, 5, 5, 5).three_of_a_kind
+    assert_equal 0, Yatzy.new(3, 3, 2, 2).three_of_a_kind
   end
 
   def test_four_of_a_kind
@@ -74,12 +121,19 @@ class YatzyTest < Test::Unit::TestCase
     assert_equal 20, Yatzy.new(5, 5, 5, 4, 5).four_of_a_kind
     assert_equal 12, Yatzy.new(3, 3, 3, 3, 3).four_of_a_kind
     assert_equal 0, Yatzy.new(3, 3, 3, 2, 2).four_of_a_kind
+
+    assert_equal 12, Yatzy.new(3, 3, 3, 3).four_of_a_kind
+    assert_equal 20, Yatzy.new(5, 5, 5, 5).four_of_a_kind
+    assert_equal 0, Yatzy.new(3, 3, 3, 2).four_of_a_kind
   end
 
   def test_small_straight
     assert_equal 15, Yatzy.new(1, 2, 3, 4, 5).small_straight
     assert_equal 15, Yatzy.new(2, 3, 4, 5, 1).small_straight
-    assert_equal 0, Yatzy.new(1, 2, 2, 4, 5).small_straight
+    assert_equal 0, Yatzy.new(2, 3, 4, 5, 6).small_straight
+
+    assert_equal 0, Yatzy.new(1, 2, 3, 4).small_straight
+    assert_equal 0, Yatzy.new(3, 4, 5, 6).small_straight
   end
 
   def test_large_straight
@@ -87,10 +141,19 @@ class YatzyTest < Test::Unit::TestCase
     assert_equal 20, Yatzy.new(2, 3, 4, 5, 6).large_straight
     assert_equal 0, Yatzy.new(1, 2, 2, 4, 5).large_straight
     assert_equal 0, Yatzy.new(1, 2, 3, 4, 5).large_straight
+
+    assert_equal 0, Yatzy.new(6, 2, 3, 4).large_straight
+    assert_equal 0, Yatzy.new(2, 3, 4, 5).large_straight
+    assert_equal 0, Yatzy.new(1, 2, 2, 4).large_straight
+    assert_equal 0, Yatzy.new(1, 2, 3, 4).large_straight
   end
 
   def test_full_house
     assert_equal 18, Yatzy.new(6, 2, 2, 2, 6).full_house
     assert_equal 0, Yatzy.new(2, 3, 4, 5, 6).full_house
+    assert_equal 0, Yatzy.new(2, 2, 2, 2, 2).full_house
+
+    assert_equal 0, Yatzy.new(6, 2, 2, 2).full_house
+    assert_equal 0, Yatzy.new(2, 3, 4, 5).full_house
   end
 end
--- ruby/yatzy.rb
+++ ruby/yatzy.rb
@@ -1,8 +1,8 @@
 class Yatzy
   attr_reader :dice
 
-  def initialize(d1, d2, d3, d4, d5)
-    @dice = [d1, d2, d3, d4, d5]
+  def initialize(d1, d2, d3, d4, d5 = nil)
+    @dice = [d1, d2, d3, d4, d5].compact
   end
 
   def chance
@@ -40,11 +40,27 @@ class Yatzy
   end
 
   def one_pair
-    find_pairs.max.to_i * 2
+    find_pairs.keys.max.to_i * 2
   end
 
   def two_pair
-    find_pairs(2).max(2).map { |die| die * 2 }.sum
+    pairs = find_pairs
+
+    return 0 if pairs.values.sum < 2
+
+    keys =  pairs.keys.sort.reverse
+
+    score = 0
+
+    keys.each do |key|
+      pair_count = pairs[key]
+
+      score += key * (2 * pair_count)
+
+      break if pair_count > 1
+    end
+
+    score
   end
 
   def three_of_a_kind
@@ -56,41 +72,49 @@ class Yatzy
   end
 
   def small_straight
-    return 15 if !dice.include?(6) && unique?
+    return 15 if !dice.include?(6) && unique? && dice.size == 5
 
     0
   end
 
   def large_straight
-    return 20 if dice.include?(6) && unique?
+    return 20 if dice.include?(6) && unique? && dice.size == 5
 
     0
   end
 
   def full_house
-    return dice.sum if one_pair > 0 && three_of_a_kind > 0
+    groups = dice.group_by { |die| die }
+
+    counts = groups.transform_values(&:size).values
+
+    return dice.sum if counts.include?(2) && counts.include?(3)
 
     0
   end
 
   private
 
+  # Sum dice with values matching the given match
   def sum(match)
     dice.select { |value| value == match }.sum
   end
 
-  def find_pairs(count = 1)
-    pairs = matches(2).keys
+  # Return map of dice values and the number of pairs each value has
+  # Ex: { 3 => 1, 2 => 1 } is one pair of threes and one pair of twos
+  def find_pairs
+    pairs = matches(2)
 
-    return [] unless pairs.size >= count
-
-    pairs
+    pairs.transform_values do |value|
+      value.size / 2
+    end
   end
 
-  def find_triples
-    matches(3)
-  end
-
+  # Return map keyed on dice numbers. Map values are individual rolls for that
+  # die.
+  #
+  # Map only includes values for dice that rolled values greater than or equal
+  # to given threshold.
   def matches(threshold)
     dice.group_by { |die| die }.select do |_, value|
       value.size >= threshold
@@ -105,6 +129,7 @@ class Yatzy
     match.first.first(value).sum
   end
 
+  # Checks if each die value is different.
   def unique?
     dice.uniq.size == dice.size
   end
